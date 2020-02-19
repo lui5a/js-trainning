c846141429a8747331c7b97c961592f9cdad42a3
Luisa Fernanda Tenorio Castilla <ltenorio@everis.com>
2020-02-19 17:55:03 +0100

    started eloquent exercises

--- src/array-methods/chapter2.js
+++ src/array-methods/chapter2.js
@@ -1,6 +1,41 @@
 
 
-function add(a, b) {
-    return a+b
-    }
-    console.log(add(4, 6))
+// console.log("This is the first line\nAnd this is the second")
+
+
+// let hash = '#'
+// while (hash.length <= 7) {
+//   console.log(hash);
+//   hash = hash + '#';
+// }
+
+// for(let number = 1; number<= 100; number += 1){
+//     if (number % 3 == 0){
+//         console.log('Fizz');
+//     }else if( number % 5 == 0){
+//         console.log('Buzz');
+//     } else console.log(number);
+// }
+
+// for(let number = 1; number<= 100; number += 1){
+//     if (number % 5 == 0 & number % 3 == 0){
+//         console.log('FizzBuzz');
+//     }else if( number % 5 == 0){
+//         console.log('Buzz');
+//     }else if (number % 3 == 0){
+//         console.log('Fizz');
+//     } else console.log(number);
+// }
+
+// for (let n = 1; n <= 100; n++) {
+//     let output = "";
+//     if (n % 3 == 0) output += "Fizz";
+//     if (n % 5 == 0) output += "Buzz";
+//     console.log(output || n);
+//   }
+
+for(let hash = '#' ; hash.length <= 8; hash += '#'){
+    console.log(hash);
+
+}
+
