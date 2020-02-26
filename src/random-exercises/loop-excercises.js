2dbbec09ccde0a9f582c4aa2399f9d0391fbc132
Luisa Fernanda Tenorio Castilla <ltenorio@everis.com>
2020-02-26 18:28:52 +0100

    code from exercise on hackerrank

--- src/random-exercises/loop-excercises.js
+++ src/random-exercises/loop-excercises.js
@@ -64,3 +64,18 @@ resetAllRandomNumbers();
 // console.log(allRandomNumbers);
 
 
+
+
+// const vowels = ['a','e','i','o','u'];
+
+//     for( let i = 0; i <= s.length ; i++){
+//         if(vowels.includes(s[i])){
+//             console.log(s[i]);
+//         }
+//     } 
+//     for( let i = 0; i <= s.length ; i++){
+//         if(!vowels.includes(s[i])){
+//             console.log(s[i]);
+//         }
+//     }
+// }
